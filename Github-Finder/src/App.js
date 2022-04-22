e96f417169abd667d78974e43898cee2280d7eb5
Sidhanth Bandodkar <bandodkarsiddhant9@gmail.com>
2022-04-23 00:59:47 +0530

    First Github-finder commit

---
+++ Github-Finder/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Home from "./Components/Home";
+import Spinner from "./Components/Spinner";
+
+const App = () => {
+  return (
+    <div>
+      
+      <Home />
+      {/* <Spinner/> */}
+      
+    </div>
+  );
+};
+
+export default App;
